6fda3ce61b3ec3c5067b90fe67e77c962e5f9816
akatsukinoyami <hirokod3@gmail.com>
2023-04-08 02:46:13 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :properties
   namespace :admin do
     resources :characters
     resources :items
     resources :properties
     resources :relatives
     resources :skills
-    resources :users, except: :index
+    resources :users
 
     root to: "characters#index"
   end
